97b86e2838d1c2b751fef7e535e883cc294f283e
Bilna <bilna.p@gmail.com>
2017-07-04 16:39:27 +0530

    Palindrome

---
+++ CodingJava/src/main/java/Palindrome.java
@@ -0,0 +1,17 @@
+/**
+ * Created by Bilna on 04-07-2017.
+ */
+public class Palindrome {
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("malaymyalam"));
+    }
+
+    public static boolean isPalindrome(String s) {
+        for (int i = 0, j = s.length() - 1; i <= s.length() / 2 & j >= s.length() / 2; i++, j--) {
+            if (!(s.charAt(i) == s.charAt(j))) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
